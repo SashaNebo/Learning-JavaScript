249bd2009ea21d8d207aba9c594340827318cf4f
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2023-11-02 19:49:04 +0300

    start work on my App

--- Деструктуризация, оператор Rest/Api/script.js
+++ Деструктуризация, оператор Rest/Api/script.js
@@ -1,11 +1,60 @@
-const link = 'http://universities.hipolabs.com/search?country=Belarus'
+const link = 'https://api.coincap.io/v2/assets'
+
+const container = document.getElementById('container')
+const form = document.getElementById('form')
+const textInput = document.getElementById('text-input')
+
+let store = {}
 
 const fetchData = async () => {
-  const result = await fetch(`${link}`)
-  const data = await result.json()
+  try {
+    const result = await fetch(`${link}`)
+    const object = await result.json()
+    const { data, timestamp } = object
 
-  console.log(result)
-  console.log(data)
+    store = { ...data }
+
+    renderContent()
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+fetchData()
+
+const renderContent = () => {
+  for (let key in store) {
+    const info = store[key]
+
+    const contentHTML = `
+    <div>
+    <span>${info.rank}.</span>
+    <span>${info.name}</span>
+    <span>${info.priceUsd}$</span>
+    </div>
+    <br>
+    `
+
+    container.insertAdjacentHTML('beforeend', contentHTML)
+  }
+}
+
+const handleInput = e => {
+  store = {
+    ...store,
+    coin: e.target.value,
+  }
+
+  console.log(store)
+}
+
+const handleSubmit = e => {
+  e.preventDefault()
+  const value = store.coin
+  console.log(value)
+}
+
+form.addEventListener('submit', handleSubmit)
+textInput.addEventListener('click', handleInput)
 
 fetchData()
