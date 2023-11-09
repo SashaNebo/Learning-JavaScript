f279c1ba4d8a2c7339079aa5df3d84957b33a272
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2023-11-09 22:07:04 +0300

    learn: promise practice2 done

--- Асинхронный JavaScript/Промисы в JavaScript [Полный курс 2023] для новичков/practice.js
+++ Асинхронный JavaScript/Промисы в JavaScript [Полный курс 2023] для новичков/practice.js
@@ -173,47 +173,185 @@ const author = {
 }
 
 // promise4 - пытаемся получить шортс автора
+// callback hell 4function.
 
-const shortId = author.shortIds[1]
+// function reqVideos(callback) {
+//   setTimeout(() => {
+//     const videos = [
+//       { id: 1, title: 'Топ 10 игр 2023' },
+//       { id: 2, title: 'Лучшая битва в Warcraft3' },
+//       { id: 3, title: 'Чем кормить кошек' },
+//     ]
+//     callback(videos)
+//   }, 1000)
+// }
 
-function f1() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const videos = [
-        { id: 1, title: 'Топ 10 игр 2023' },
-        { id: 2, title: 'Лучшая битва в Warcraft3' },
-        { id: 3, title: 'Чем кормить кошек' },
-      ]
-      resolve(videos)
-    }, 1000)
-  })
-}
+// function reqDescription(callback) {
+//   setTimeout(() => {
+//     const description = {
+//       id: 1,
+//       title: 'Топ 10 игр 2023',
+//       hashTags: ['игры', '2023'],
+//       authorId: 55,
+//     }
+//     callback(description)
+//   }, 1000)
+// }
 
-function f2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const id = videos[0].id
+// function reqAuthor(callback) {
+//   setTimeout(() => {
+//     const author = {
+//       id: 55,
+//       name: 'Какие-то уроки',
+//       videoIds: [1, 2, 3],
+//       shortIds: [10, 15, 33],
+//     }
+//     callback(author)
+//   }, 1000)
+// }
 
-      const description = {
-        id: 1,
-        title: 'Топ 10 игр 2023',
-        hashTags: ['игры', '2023'],
-        authorId: 55,
-      }
-      resolve(description)
-    }, 1000)
-  })
-}
+// function reqShots(callback) {
+//   setTimeout(() => {
+//     const author = {
+//       id: 55,
+//       name: 'Какие-то уроки',
+//       videoIds: [1, 2, 3],
+//       shortIds: [10, 15, 33],
+//     }
+//     callback(author.shortIds)
+//   }, 1000)
+// }
 
-function start() {
-  f1()
-    .then(videos => {
-      console.log(videos)
-      return f2(description)
-    })
-    .then(d => {
-      console.log(d)
-    })
-}
+// function getData() {
+//   reqVideos(videos => {
+//     console.log(videos)
+//     reqDescription(description => {
+//       console.log(description)
+//       reqAuthor(author => {
+//         console.log(author)
+//         reqShots(shorts => {
+//           console.log(shorts)
+//         })
+//       })
+//     })
+//   })
+// }
 
-start()
+// getData()
+
+// function reqVideos() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const videos = [
+//         { id: 1, title: 'Топ 10 игр 2023' },
+//         { id: 2, title: 'Лучшая битва в Warcraft3' },
+//         { id: 3, title: 'Чем кормить кошек' },
+//       ]
+//       resolve(videos)
+//     }, 1000)
+//   })
+// }
+
+// function reqDescription() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const description = {
+//         id: 1,
+//         title: 'Топ 10 игр 2023',
+//         hashTags: ['игры', '2023'],
+//         authorId: 55,
+//       }
+//       resolve(description)
+//     }, 1000)
+//   })
+// }
+
+// function reqAuthor() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const author = {
+//         id: 55,
+//         name: 'Какие-то уроки',
+//         videoIds: [1, 2, 3],
+//         shortIds: [10, 15, 33],
+//       }
+//       resolve(author)
+//     }, 1000)
+//   })
+// }
+
+// function reqShots() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const author = {
+//         id: 55,
+//         name: 'Какие-то уроки',
+//         videoIds: [1, 2, 3],
+//         shortIds: [10, 15, 33],
+//       }
+//       resolve(author.shortIds)
+//     }, 1000)
+//   })
+// }
+
+// function getData() {
+//   reqVideos()
+//     .then(videos => {
+//       console.log(videos)
+//       return reqDescription(videos[0])
+//     })
+//     .then(description => {
+//       console.log(description)
+//       return reqAuthor()
+//     })
+// .then(author => {
+//   console.log(author)
+//   return reqShots()
+// })
+// .then(shorts => {
+//   console.log(shorts)
+// })
+// .catch(() => {
+//   console.log('...')
+// })
+// .finally(() => {
+//   console.log('...')
+// })
+// }
+
+// function getData2() {
+//   Promise.all([reqVideos(), reqDescription(), reqAuthor(), reqShots()]).then(data => {
+//     setTimeout(() => {
+//       console.log(data)
+//     }, 5000)
+//   })
+// }
+
+// getData()
+// // getData2()
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const videos = [
+      { id: 1, title: 'Топ 10 игр 2023' },
+      { id: 2, title: 'Лучшая битва в Warcraft3' },
+      { id: 3, title: 'Чем кормить кошек' },
+    ]
+    resolve(videos)
+  }, 666)
+})
+
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const videos = [
+//       { id: 1, title: 'Топ 10 игр 2023' },
+//       { id: 2, title: 'Лучшая битва в Warcraft3' },
+//       { id: 3, title: 'Чем кормить кошек' },
+//     ]
+//     resolve(videos)
+//   }, 666)
+// })
+
+promise1.then(videos => {
+  videos.forEach(v => console.log(v))
+})
