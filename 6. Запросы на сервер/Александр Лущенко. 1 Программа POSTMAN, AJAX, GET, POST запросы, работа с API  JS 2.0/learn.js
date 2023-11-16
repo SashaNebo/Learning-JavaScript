b1ece30788f6e841dbcfdf9bec86d5509747b43b
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2023-11-16 21:26:47 +0300

    learn: request to server 1; YT Alex lushenko

---
+++ 6. Запросы на сервер/Александр Лущенко. 1 Программа POSTMAN, AJAX, GET, POST запросы, работа с API  JS 2.0/learn.js
@@ -0,0 +1,20 @@
+// const url = 'https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact'
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '8362923709msh0ab0109d10d59c2p1a22e1jsn6059d82a0f56',
+//     'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+//   },
+// }
+
+// async function getData() {
+//   try {
+//     const response = await fetch(url, options)
+//     const result = await response.json()
+//     console.log(result)
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// getData()
